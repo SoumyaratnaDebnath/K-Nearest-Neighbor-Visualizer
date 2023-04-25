9b171c9bc83d580feddf731157d2d308561dd41d
Soumyaratna Debnath <68662972+SoumyaratnaDebnath@users.noreply.github.com>
2023-04-25 18:37:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -63,8 +63,8 @@ def keffect(k):
     mse, bias, var = bias_variance_decomp(knn, X_train, y_train, X_test, y_test, loss='mse', num_rounds=200, random_seed=1)
     
     # Create a meshgrid for decision boundary plotting
-    a = np.arange(start=X_train[:,0].min()-1, stop=X_train[:,0].max()+1, step=0.01)
-    b = np.arange(start=X_train[:,1].min()-1, stop=X_train[:,1].max()+1, step=0.01)
+    a = np.arange(start=X_train[:,0].min()-1, stop=X_train[:,0].max()+1, step=selected_step)
+    b = np.arange(start=X_train[:,1].min()-1, stop=X_train[:,1].max()+1, step=selected_step)
     XX, YY = np.meshgrid(a, b)
     input_array = np.array([XX.ravel(), YY.ravel()]).T
     labels = knn.predict(input_array)
@@ -128,7 +128,7 @@ with st.sidebar:
     # selected_alpha = st.sidebar.slider('Select the transparency of the decision boundary', min_value=0.0, max_value=1.0, value=0.5, step=0.1)
     fig_width, fig_height = 10, 6
     selected_alpha = 0.5
-    selected_step = st.sidebar.slider('Select the stepsize for the grid', min_value=1, max_value=10, value=5, step=1)
+    selected_step = st.sidebar.slider('Select the stepsize for the grid', min_value=1, max_value=10, value=5, step=1) * 0.01/5
     
     st.write("---")
     st.sidebar.header("Data Settings")
